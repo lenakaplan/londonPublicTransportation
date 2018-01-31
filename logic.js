eb8c6cf0d07afdf9082bd80ecbd947179ee24aab
Lena Kaplan <lenak@traxretail.com>
2018-01-31 22:44:46 +0200

    get nearby stops and return the next departure time between from and to stops

--- logic.js
+++ logic.js
@@ -3,14 +3,12 @@ const request = require('request');
 const axios = require('axios'); 
 const GET_JOURNEY = 'https://api.tfl.gov.uk/Journey/JourneyResults';
 const GET_NEAREST_STOPS = 'https://api.tfl.gov.uk/StopPoint';
-
+const STOP_TYPES = `NaptanMetroStation,NaptanRailStation,NaptanBusCoachStation,NaptanFerryPort,NaptanPublicBusCoachTram`;
 function getNearestStops(location){
-    let locationArr = location.split();
-    let lat= locationarr[0];
-    let lon=locationArr[1];
-    let stopTypes=`NaptanMetroStation,NaptanRailStation,NaptanBusCoachStation,NaptanFerryPort,NaptanPublicBusCoachTram`;
-    let url = `${url}?lat=${lat}&lon=${lon}&stoptypes=${stopTypes}`;
-    console.log(url);
+    let locationArr = location.split(',');
+    let lat = locationArr[0];
+    let lon =locationArr[1];
+    let url = `${GET_NEAREST_STOPS}?lat=${lat}&lon=${lon}&stoptypes=${STOP_TYPES}`;
     return axios(url);
 }
 
@@ -18,15 +16,18 @@ module.exports = {
     async getDepratingTime(from, to, callback){
         let startPoint  = await getNearestStops(from);
         let endPoint  = await getNearestStops(to);
-        console.log(startPoint);
-        console.log(endPoint);
-        let url = `GET_JOURNEY/${startPoint}/to/${endPoint}?timeIs=Departing`;
-        console.log(url);
+        let startId = startPoint.data.stopPoints[0].id;
+        let endId = endPoint.data.stopPoints[0].id;
+        let url = `${GET_JOURNEY}/${startId}/to/${endId}?timeIs=Departing`;
         request(url, function(err, response, body){
+            if (err){
+                callback();
+                return;
+            }
 	        let obj = JSON.parse(response.body);
 	        console.log(obj.searchCriteria.dateTime);
 	        callback(null, obj.searchCriteria.dateTime);
         });
     }
-}
+};
 
